d1bbdc110961f0accc1618194fd900cecc88317b
David Wolffberg <1350533+wolffberg@users.noreply.github.com>
2022-09-29 12:56:03 +0000

    feat: block public access to s3 bucket and encrypt dynamodb table
    both changes are needed to keep terragrunt from trying to correct what it deems unsafe permissions

--- bootstrap/remote-state/shared/main.tf
+++ bootstrap/remote-state/shared/main.tf
@@ -7,8 +7,6 @@ terraform {
   }
 }
 
-provider "aws" {}
-
 locals {
   # Try to convert repository name with hyphens into CamcelCase
   camelcase_role_name = try(replace(title(replace(var.repository_name, "-", " ")), " ", ""), var.repository_name)
@@ -16,15 +14,90 @@ locals {
 
 resource "aws_s3_bucket" "this" {
   bucket = "${var.repository_name}-remote-state"
-  acl    = "private"
-
-  versioning {
-    enabled = true
-  }
 
   tags = {
     Name = var.repository_name
   }
+}
+
+resource "aws_s3_bucket_acl" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  acl = "private"
+}
+
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  rule {
+    # TODO: Fix this
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+    bucket_key_enabled = true
+  }
+}
+
+# These statements are required as Terragrunt 
+# will otherwise try to add it on each apply.
+data "aws_iam_policy_document" "s3" {
+  statement {
+    sid     = "RootAccess"
+    actions = ["s3:*"]
+
+    resources = [
+      aws_s3_bucket.this.arn
+    ]
+
+    principals {
+      type        = "AWS"
+      identifiers = [data.aws_caller_identity.current.account_id]
+    }
+  }
+
+  statement {
+    sid     = "EnforceSecureTransport"
+    effect  = "Deny"
+    actions = ["s3:*"]
+
+    resources = [
+      aws_s3_bucket.this.arn,
+      "${aws_s3_bucket.this.arn}/*"
+    ]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = [false]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "this" {
+  bucket = aws_s3_bucket.this.id
+  policy = data.aws_iam_policy_document.s3.json
 }
 
 resource "aws_dynamodb_table" "this" {
@@ -32,6 +105,10 @@ resource "aws_dynamodb_table" "this" {
   hash_key       = "LockID"
   read_capacity  = 5
   write_capacity = 5
+
+  server_side_encryption {
+    enabled = true
+  }
 
   attribute {
     name = "LockID"
@@ -56,13 +133,17 @@ data "aws_iam_policy_document" "assume" {
   }
 }
 
+data "aws_caller_identity" "current" {}
+
 data "aws_iam_policy_document" "tfstate" {
   statement {
     sid = "S3ListBucket"
 
     actions = [
       "s3:ListBucket",
-      "s3:GetBucketVersioning"
+      "s3:GetBucketVersioning",
+      "s3:GetBucketPublicAccessBlock",
+      "s3:GetBucketPolicy"
     ]
 
     resources = [
